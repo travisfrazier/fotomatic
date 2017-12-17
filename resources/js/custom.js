d06e81f80ef179c413e533f2b354d5c1b5728f33
travisfrazier <travisf777@gmail.com>
2017-12-17 17:34:31 -0500

    new commit

---
+++ resources/js/custom.js
@@ -0,0 +1,28 @@
+window.sr = ScrollReveal({ reset: true });
+
+// Custom Settings
+sr.reveal('.foo-1', { duration: 900 });
+
+sr.reveal('.foo-2', { 
+  duration: 1200
+});
+
+sr.reveal('.foo-3', { 
+  duration: 1500
+});
+
+sr.reveal('.foo-4', { 
+  duration: 1800,
+});
+
+sr.reveal('.foo-5', { 
+  duration: 2100 
+});
+
+sr.reveal('.foo-6', { 
+    duration: 2400 
+});
+
+sr.reveal('.foo', {
+    duration: 1000
+});
