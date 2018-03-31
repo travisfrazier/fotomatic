4a953f1d2b58236b0e71f9d4b1e9c8ddfff40e96
travisfrazier <travisf777@gmail.com>
2018-03-31 15:37:43 -0400

    updated css and javascript

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,19 +1,24 @@
-// Modal Sign Up Form and Validation
+// modal pop up form
 const buttonOne = document.querySelector('#button');
 const buttonTwo = document.querySelector('#buttonTwo');
+const close = document.querySelector('#close');
 const modalContent = document.querySelector('.modal-container');
 
-
+// open modal desktop and mobile buttons
 buttonOne.onclick = function() {
   modalContent.style.display = 'block';
 };
  
 buttonTwo.onclick = function() {
-  console.log('hey');
   modalContent.style.display = 'block';
 };
 
-// Email Validation 
+// close modal form 
+close.onclick = function() {
+  modalContent.style.display = 'none';
+}
+
+// email sign up validation
 function emailValidate() {
   const email = document.forms["email-form"]["email"].value.trim();
 
@@ -23,42 +28,42 @@ function emailValidate() {
   }
 }
 
-// Image Slider begin
-let sliderImages = document.querySelectorAll('.sign-up-section');
-let arrowLeft = document.querySelector('#arrow-left');
-let arrowRight = document.querySelector('#arrow-right');
-// Represent what slide is displaying 
+// image slider variables 
+const sliderImages = document.querySelectorAll('.sign-up-section');
+const arrowLeft = document.querySelector('#arrow-left');
+const arrowRight = document.querySelector('#arrow-right');
+// represent what slide is displaying 
 let current = 0;
 
 
-//Sets all slides to display none
+//sets all slider images to display none
 function reset() {
   for (let i = 0; i < sliderImages.length; i++) {
     sliderImages[i].style.display = 'none';
   }
 }
 
-// Starts slider
+// starts slider and sets first image to display
 function startSlide() {
   reset();
   sliderImages[0].style.display = 'block'; 
 }
 
-// Previous slide 
+// previous slide 
 function slideLeft() {
   reset();
   sliderImages[current - 1].style.display = 'block';
   current--;
 }
 
-// Next slide 
+// next slide 
 function slideRight() {
   reset();
   sliderImages[current + 1].style.display = 'block';
   current++;
 }
 
-//Move left
+// move left
 arrowLeft.addEventListener('click', function() {
   if (current === 0) {
     current = sliderImages.length;
@@ -66,7 +71,7 @@ arrowLeft.addEventListener('click', function() {
   slideLeft();
 });
 
-// Move right
+// move right
 arrowRight.addEventListener('click', function() {
   if (current === sliderImages.length - 1) {
     current = -1;
@@ -75,6 +80,4 @@ arrowRight.addEventListener('click', function() {
 });
 
 startSlide();
-
-// End image slider 
-
+// end of image slider 
